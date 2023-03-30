1e2ef32a7e441cc3e8e5670a7d6e97c58689b672
silvialep <silvia.lepore_sl@libero.it>
2023-03-30 13:12:01 +0200

    feat: init api get

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,51 @@
 <script>
 
+import {store} from './store.js'
+
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      store,
+    }
+
+  },
+
+  methods: {
+    
+  },
+  
+  emits: [
+
+  ],
+
+  components: {
+
+  },
+
+
+  computed: {
+
+  },
+
+  created() {
+    axios.get(this.store.APIcall).then((res) => {
+
+      let newSearch = this.store.APIcall += '&query=';
+
+      console.log(newSearch);
+
+      // this.store.movies = res.data.data;
+
+
+    })
+  }
+
+
+
+}
+
 </script>
 
 <template>
---
+++ src/store.js
@@ -0,0 +1,10 @@
+import {reactive} from 'vue';
+
+export const store = reactive({
+
+    //API key c2c3969c50cd0fbe2f04ca8885d83f85
+
+    APIcall: 'https://api.themoviedb.org/3/search/movie?api_key=c2c3969c50cd0fbe2f04ca8885d83f85',
+
+    movies: [],
+})
